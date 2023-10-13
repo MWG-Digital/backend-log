bda4653131f2fc683eaf75fa94ed8d722326a232
Israel Melicio <israel@mwg.digital>
2023-10-12 21:32:55 -0300

    Apply new codeStyle

--- backendLog.js
+++ backendLog.js
@@ -1,35 +1,39 @@
 class Console {
   static _logLevelValidation() {
-    return process.env.LOG_LEVEL != undefined ? process.env.LOG_LEVEL : "ERROR";
+    return process.env.LOG_LEVEL !== undefined ? process.env.LOG_LEVEL : 'ERROR'
   }
 
   static _print(param1, param2) {
     if (param2 == undefined) {
-      console.info(param1);
+      console.info(param1)
     } else {
-      console.info(param1, param2);
+      console.info(param1, param2)
     }
   }
 
   static info(message, item) {
-    let LOG_LEVEL = this._logLevelValidation();
-    if (LOG_LEVEL != "OFF" && LOG_LEVEL != "ERROR" && LOG_LEVEL != "WARNING") {
-      this._print(message, item);
+    const LOG_LEVEL = this._logLevelValidation()
+    if (
+      LOG_LEVEL !== 'OFF' &&
+      LOG_LEVEL !== 'ERROR' &&
+      LOG_LEVEL !== 'WARNING'
+    ) {
+      this._print(message, item)
     }
   }
 
   static payload(message, item) {
-    let LOG_LEVEL = this._logLevelValidation();
-    if (LOG_LEVEL != "OFF" && LOG_LEVEL != "ERROR") {
-      this._print(message, item);
+    const LOG_LEVEL = this._logLevelValidation()
+    if (LOG_LEVEL !== 'OFF' && LOG_LEVEL !== 'ERROR') {
+      this._print(message, item)
     }
   }
 
   static error(message, item) {
-    let LOG_LEVEL = this._logLevelValidation();
-    if (LOG_LEVEL != "OFF") {
-      this._print(message, item);
+    const LOG_LEVEL = this._logLevelValidation()
+    if (LOG_LEVEL !== 'OFF') {
+      this._print(message, item)
     }
   }
 }
-module.exports.Console = Console;
+module.exports.Console = Console
